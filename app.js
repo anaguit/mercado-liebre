8f846aac0a26a5ea3ca0c3351d4edfbe556ff62d
anaguit <anasantoandre84@gmail.com>
2021-07-12 11:30:55 -0300

    ultimo

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req,res) =>{
+   res.sendFile(path.join(__dirname, './views/index.html'));  // Permite enviar un archivo HTML
+});
+
+app.get('/register', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/register.html')); 
+ });
+
+app.get('/login', (req,res) =>{
+    res.sendFile(path.join(__dirname, './views/login.html'));  
+ });
+app.use(express.static(path.join(__dirname, './public')));  
+
+app.listen(3000, () => {
+    console.log("Servidor-corriendo");
+});
+
+
+
